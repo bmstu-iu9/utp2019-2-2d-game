896cfe41781e779c889c7e3beeac33dbe1a41118
BuildTools <unconfigured@null.spigotmc.org>
2019-07-03 23:17:28 +0300

    Update example

--- example.js
+++ example.js
@@ -6,6 +6,7 @@ image.onload = () => {
 	const r = new Render(image);
 	let t = [];
 	t.push(r.createObject([1, 1], [2, 2], [0, 0], [1, 1], 5));
-	t.push(r.createObject([2.5, 2.5], [4, 4], [0, 0], [1, 1], 5));
+	t.push(r.createObject([2.5, 2.5], [4, 4], [0, 0], [0.5, 1], 5));
+	t.push(r.createObject([6.5, 2.5], [8, 4], [0.5, 0], [1, 1], 5));
 	r.render(1, 0, 6, t);
 };

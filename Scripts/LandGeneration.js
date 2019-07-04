45d870baa30a6a5bfb1594632934f5336f6e2fcf
Svetlana Pirozhkova <svetiklanka.2000@yandex.ru>
2019-07-04 17:48:34 +0500

    Update LandGeneration.js
    Улучшенная генерация поверхности

--- Scripts/LandGeneration.js
+++ Scripts/LandGeneration.js
@@ -1,16 +1,25 @@
-// Генерация массива уровней поверхности
-const landGen = (minHeight, maxHeight, widthWorld) => {
+const landGen = (minHeight, maxHeight, widthWorld, heightWorld) => {
     let heights = [Math.floor((maxHeight + minHeight) / 2)];
     let i = 1;
-
+    let lastSign = 0;
     while (i < widthWorld) {
-        let sectionLength = Math.round(Math.random() * 7) + 1; // Длина сектора возрастания или убывания
-        let sign = Math.floor(Math.random() * 2);
+        let sectionLength = Math.round(random() * 9) + 1; // Длина сектора возрастания или убывания
+        let sign = Math.floor(random() * 2);
         if (sign === 0) sign = -1;
         i += sectionLength;
-
+        let lastDelta = 1;
+        if(lastSign != sign){
+            lastDelta = 0;
+        }
+        lastSign = sign;
         while (sectionLength > 0) {
-            let delta = Math.ceil(Math.random() * 3); // Максимальная разница в уровнях = 2
+            let delta = 0;
+            if(lastDelta == 0 && random() > 0.07){
+                delta = 0;
+            }else{
+                delta = Math.ceil(random() * 3); // Максимальная разница в уровнях = 2
+            }
+            lastDelta = delta;
             let nextHeight = heights[heights.length - 1] + delta * sign;
             if ((nextHeight >= maxHeight) || (nextHeight <= minHeight)) {
                 i -= sectionLength;
@@ -19,6 +28,13 @@ const landGen = (minHeight, maxHeight, widthWorld) => {
             sectionLength--;
         }
     }
-
-    return heights;
+    // Преобразование карты высот в матрицу
+    let arr = new Array;
+    for(let x = 0; x < widthWorld; x++){
+        arr[x] = new Array;
+        for(let y = 0; y < heightWorld; y++){
+            arr[x][y] = (y <= heights[x]);
+        }
+    }
+    return arr;
 }

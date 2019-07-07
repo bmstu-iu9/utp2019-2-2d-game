8a8a353e289ef2c7c89f6b855a49d5eecb9c5de2
Whitequartz <nadim.abdulgani@mail.ru>
2019-07-08 01:03:51 +0300

    Добавлена освещённость фона

--- Scripts/GameArea.js
+++ Scripts/GameArea.js
@@ -40,7 +40,7 @@ class GameArea{
 		// Возвращает освещение конкретного блока
 		this.getLight = (x, y) => {
 			let grad = (y > 0.5 * height) ? 1 : ((y < 0.3 * height) ? 0.2 : ((y - 0.3 * height) / (0.2 * height) * 0.8 + 0.2));
-			let k = Math.min(0.5 + this.timeOfDay / 2, grad);
+			let k = Math.min(1 / 3 + this.timeOfDay * 3 / 2, grad);
 			let light = Math.max(Math.floor(shadowMap[x][y] / 1000) * 1000, shadowMap[x][y] % 1000) / 9;
 			return k * light;
 		}

60cfbe30cf711467f080d711c678ed12c5fb297f
Whitequartz <nadim.abdulgani@mail.ru>
2019-07-07 23:57:54 +0300

    Добавлена освещённость блоков

--- Scripts/level.js
+++ Scripts/level.js
@@ -6,9 +6,8 @@ const chankWidth = 8, chankHeight = 8   Размеры чанка
 const minLayout = 2, maxLayout = 3      Обрабатываемые слои
 const blockResolution = 32              Разрешение текстуры блока
 let deltaTime = 0                       Изменение времени между кадрами в мс
-let blocks;                             Игровой мир (объект GameArea)
+let gameArea;                           Игровой мир (объект GameArea)
 cameraSet(x, y)                         Устанавливает ккординаты камеры на (x, y)
-elevationMap							Карта высот мира
 */
 
 
@@ -16,18 +15,18 @@ elevationMap							Карта высот мира
 const key = performance.now();  // Ключ генерации
 let _x = 0, _y = 0;
 const beginPlay = () => {  // Вызывается только при запуске
-    blocks = generate(1024, 1024, key);
+    gameArea = generate(1024, 1024, key);
 	_x = 0;
-	_y = elevationMap[_x];
+	_y = gameArea.elevationMap[_x];
 }
 
 
 const eventTick = () => {  // Вызывается каждый кадр
 	let speed = 3;
-	if(_x >= blocks.width - 1){
+	if(_x >= gameArea.width - 1){
 		_x = 1;
-		_y = elevationMap[_x];
+		_y = gameArea.elevationMap[_x];
 	}
-	let targetX = Math.floor(_x) + 2, targetY = elevationMap[targetX];
+	let targetX = Math.floor(_x) + 2, targetY = gameArea.elevationMap[targetX];
     cameraSet(_x += speed * (targetX - _x) * deltaTime / 1000, _y += speed * (targetY - _y) * deltaTime / 1000)
 }

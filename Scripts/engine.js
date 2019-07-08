61fb8f64822f0f83d2f5c9a26aca5070892daff3
Whitequartz <nadim.abdulgani@mail.ru>
2019-07-08 17:46:18 +0300

    Update documentation

--- Scripts/engine.js
+++ Scripts/engine.js
@@ -49,7 +49,7 @@ r.render(x, y, xp, yp, scale, arrayOfObjects)
 			light - освещённость слоя [0..1]
 			xc, yc - координаты чанка
 
-Полный рабочий пример (устарел):
+Полный рабочий пример:
 
 const image = new Image();
 image.src = 'Images/image.png';
@@ -57,33 +57,37 @@ image.onload = () => {
 	const background = new Image();
 	background.src = 'Images/background.png';
 	background.onload = () => {
-		const r = new Render(image, background);
-		
-		r.settings(32, 4, 3);
-		
-		r.createObjects(
-			[{'id':1, 'a':[32.5/128, 32.5/128], 'b':[63.5/128, 63.5/128]},
-			{'id':3, 'a':[0.5/128, 0.5/128], 'b':[31.5/128, 31.5/128]}]);
-		
-        let arrayOfChunk = [{
-			'chunk':
-				[[1,1,1,1],
-				[1,1,3,3],
-				[1,1,1,1]],
-			'slice': 1,
-			'x': 0,
-			'y': 0
-			}];
-		let e = -20;
-		let oldtime = 0;
-		const update = (newtime) => {
-			newtime *= 0.005;
-			const deltaTime = newtime - oldtime;
-			oldtime = newtime;
-			r.render(e += deltaTime, 0, 1, arrayOfChunk);
+		const playerImage = new Image();
+		playerImage.src = 'Images/player.png';
+		playerImage.onload = () => {
+			const r = new Render(image, background, playerImage);
+			
+			r.settings(32, 4, 3);
+			
+			r.createObjects(
+				[{'id':1, 'a':[32.5/128, 32.5/128], 'b':[63.5/128, 63.5/128]},
+				{'id':3, 'a':[0.5/128, 0.5/128], 'b':[31.5/128, 31.5/128]}]);
+			
+			let arrayOfChunk = [{
+				'chunk':
+					[[1,1,1,1],
+					[1,1,3,3],
+					[1,1,1,1]],
+				'slice': 1,
+				'x': 0,
+				'y': 0
+				}];
+			let e = -20;
+			let oldtime = 0;
+			const update = (newtime) => {
+				newtime *= 0.005;
+				const deltaTime = newtime - oldtime;
+				oldtime = newtime;
+				r.render(e += deltaTime, 0, e, 0, 1, arrayOfChunk);
+				requestAnimationFrame(update);
+			}
 			requestAnimationFrame(update);
-		}
-		requestAnimationFrame(update);
+		};
     };
 };
 

c043d8c712b9528d42a7c51dc732a520b1fec59c
Shaniser <mr.shaniser@gmail.com>
2019-07-11 01:10:19 +0300

    Refactored controller.js

--- Scripts/controller.js
+++ Scripts/controller.js
@@ -1,38 +1,39 @@
 class Controller {
 
-  constructor() {
-    this.down  = new ButtonInput();
-    this.left  = new ButtonInput();
-    this.right = new ButtonInput();
-    this.up    = new ButtonInput();
-  }
-
-  keyDownUp(event) {
-
-    const down = event.type == "keydown";
-
-    switch(event.keyCode) {
-
-      case 65: this.left.getInput(down);  break;
-      case 87: this.up.getInput(down);    break;
-      case 68: this.right.getInput(down); break;
-      case 83: this.down.getInput(down);
-
+    constructor() {
+        this.down   = new ButtonInput();
+        this.left   = new ButtonInput();
+        this.right  = new ButtonInput();
+        this.up     = new ButtonInput();
     }
 
-  }
-
-};
+    keyDownUp(event) {
+        const down = event.type == "keydown"; 
+        switch(event.keyCode) {
+            case 65: 
+                this.left.getInput(down);
+                break;
+            case 87:
+                this.up.getInput(down);
+                break;
+            case 68:
+                this.right.getInput(down);
+                break;
+            case 83:
+                this.down.getInput(down);
+                break;
+        }
+    }
+}
 
 class ButtonInput {
 
-  constructor() {this.active = this.down = false;}
+    constructor() {
+        this.active = this.down = false;
+    }
 
-  getInput(down) {
-
-    if (this.down != down) this.active = down;
-    this.down = down;
-
-  }
-
-};
+    getInput(down) {    
+        if (this.down != down) this.active = down;
+        this.down = down; 
+    }
+}

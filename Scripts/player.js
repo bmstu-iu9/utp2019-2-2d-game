fe2c1aa5ba1287bdb006cfee30995d901d8bc62e
Shaniser <mr.shaniser@gmail.com>
2019-07-11 15:24:36 +0300

    Добавлены прыжки, корректное передвижение
    Движение на WASD, при удержании прыжок выше
    
    Рефакторинг некоторых фрагментов

--- Scripts/player.js
+++ Scripts/player.js
@@ -1,5 +1,5 @@
 class Player {
-    constructor(gamearea, x, y) {
+    constructor(x, y) {
         // Задаем положение игрока
         this.x = x;
         this.y = y;
@@ -15,7 +15,7 @@ class Player {
         this.hand = undefined;
 
         this.inActionRadius = (x, y) => {
-            if (x < 0 || y < 0 || x >= gamearea.width || y >= gamearea.height) return false; // проверка на выход из карты
+            if (x < 0 || y < 0 || x >= gameArea.width || y >= gameArea.height) return false; // проверка на выход из карты
             let dx = x - this.x;
             let dy = y - this.y;
             return Math.floor(Math.sqrt(dx * dx + dy * dy)) <= Player.ACTION_RADIUS;
@@ -29,22 +29,22 @@ class Player {
                     lt = this.hand.layout; // Блоки какого уровня добывает инструмент
                     type = this.hand.toolType; // блоки какого типа добывает инструмент
                 } else {
-                    lt = GameArea.MAIN_LAYOUT;
+                    lt = gameArea.MAIN_LAYOUT;
                     type = undefined; // Если в руках не инструмент
                 }
-                let blockType = gamearea.map[x][y][lt]; // Тип блока
+                let blockType = gameArea.map[x][y][lt]; // Тип блока
                 if (type === blockType) {
-                    this.inv.push(gamearea.goodDestroy(x, y, lt)); // вставляет лут в инвентарь - пока что сразу
-                } else gamearea.destroyBlock(x, y, lt);
+                    this.inv.push(gameArea.goodDestroy(x, y, lt)); // вставляет лут в инвентарь - пока что сразу
+                } else gameArea.destroyBlock(x, y, lt);
             }
         };
 
         this.place = (x, y) => {
             // Пока ставим только в MAIN_LAYOUT
             if (this.hand && !this.hand.isTool && this.inActionRadius(x, y)) {
-                let id = gamearea.map[x][y][GameArea.MAIN_LAYOUT]; // id того, что там сейчас
+                let id = gameArea.map[x][y][gameArea.MAIN_LAYOUT]; // id того, что там сейчас
                 if (id === undefined || !blockTable[id].isCollissed) {
-                    gamearea.placeBlock(x, y, GameArea.MAIN_LAYOUT, this.hand.id);
+                    gameArea.placeBlock(x, y, gameArea.MAIN_LAYOUT, this.hand.id);
                     let ind = this.inv.indexOf(this.hand);
                     if (ind > - 1) this.inv.splice(ind, 1);
 					this.hand = undefined;
@@ -56,14 +56,14 @@ class Player {
             // Если можно взаимодействовать - сделать это
             // Пока взаимодействуем только в MAIN_LAYOUT
             if (this.inActionRadius(x, y)) {
-                gamearea.interactWithBlock(x, y, GameArea.MAIN_LAYOUT);
+                gameArea.interactWithBlock(x, y, gameArea.MAIN_LAYOUT);
             }
         };
 
         // Получение информации о блоке, пока только в MAIN_LAYOUT
         this.info = (x, y) => {
             if (this.inActionRadius(x, y)) {
-                let block = blockTable[gamearea.map[x][y][GameArea.MAIN_LAYOUT]];
+                let block = blockTable[gameArea.map[x][y][gameArea.MAIN_LAYOUT]];
                 alert(`Block on ${x} ${y} : ` + JSON.stringify(block));
             }
         };
@@ -87,60 +87,63 @@ class Player {
         };
 
 
-        // Функции, проверяющие доступность движения по направлениям
-        this.checkRightCol = (newX, newY) => {
-            for (let y = Math.ceil(newY + Player.HEIGHT); y > Math.floor(newY); y--) {
-                if (gamearea.map[Math.floor(newX + Player.HALF_WIDTH)][y][GameArea.MAIN_LAYOUT] &&
-                    (blockTable[gamearea.map[Math.floor(newX + Player.HALF_WIDTH)][y][GameArea.MAIN_LAYOUT]] === undefined ||
-                    blockTable[gamearea.map[Math.floor(newX + Player.HALF_WIDTH)][y][GameArea.MAIN_LAYOUT]].isCollissed)) {
-                    return false;
-                }
-            }
-            return true;
-        };
+        // Проверка коллизии граней игрока с блоками
 
-        this.checkLeftCol = (newX, newY) => {
-            for (let y = Math.ceil(newY + Player.HEIGHT); y > Math.floor(newY); y--) {
-                if (gamearea.map[Math.floor(newX - Player.HALF_WIDTH)][y][GameArea.MAIN_LAYOUT] &&
-                    (blockTable[gamearea.map[Math.floor(newX - Player.HALF_WIDTH)][y][GameArea.MAIN_LAYOUT]] === undefined ||
-                    blockTable[gamearea.map[Math.floor(newX - Player.HALF_WIDTH)][y][GameArea.MAIN_LAYOUT]].isCollissed)) {
-                    return false;
+        // Задевает ли левая грань игрока блоки с коллизией
+        this.isCollisionLeft = (newX, newY) => {
+            let i = Math.floor(newX - Player.WIDTH / 2);
+            for(let j = Math.floor(newY); j < Math.ceil(newY + Player.HEIGHT); j++) {
+                if(gameArea.hasCollision(i, j, GameArea.MAIN_LAYOUT)){
+                    return true;
                 }
             }
-            return true;
-        };
-
-        this.checkUpCol = (newX, newY) => {
-            for (let x = newX - Player.HALF_WIDTH; Math.floor(x) <= Math.floor(newX + Player.HALF_WIDTH); x++) {
-                if (gamearea.map[Math.floor(x)][Math.ceil(newY + Player.HEIGHT)][GameArea.MAIN_LAYOUT] &&
-                    (blockTable[gamearea.map[Math.floor(x)][Math.ceil(newY + Player.HEIGHT)][GameArea.MAIN_LAYOUT]] === undefined ||
-                    blockTable[gamearea.map[Math.floor(x)][Math.ceil(newY + Player.HEIGHT)][GameArea.MAIN_LAYOUT]].isCollissed)) {
-                    return false;
-                }
-            }
-            return true;
-        };
-
-        this.checkDownCol = (newX, newY) => {
-            for (let x = newX - Player.HALF_WIDTH; Math.floor(x) <= Math.floor(newX + Player.HALF_WIDTH); x++) {
-                if (gamearea.map[Math.floor(x)][Math.floor(newY + 1)][GameArea.MAIN_LAYOUT] &&
-                    (blockTable[gamearea.map[Math.floor(x)][Math.floor(newY + 1)][GameArea.MAIN_LAYOUT]] === undefined ||
-                    blockTable[gamearea.map[Math.floor(x)][Math.floor(newY + 1)][GameArea.MAIN_LAYOUT]].isCollissed)) {
-                    return false;
-                }
-            }
-            return true;
+            return false;
         }
 
+        // Задевает ли правая грань игрока блоки с коллизией
+        this.isCollisionRight = (newX, newY) => {
+            let i = Math.floor(newX + Player.WIDTH / 2);
+            for(let j = Math.floor(newY); j < Math.ceil(newY + Player.HEIGHT); j++) {
+                if(gameArea.hasCollision(i, j, GameArea.MAIN_LAYOUT)){
+                    return true;
+                }
+            }
+            return false;
+        }
+
+        // Задевает ли верхняя грань игрока блоки с коллизией
+        this.isCollisionUp = (newX, newY) => {
+            let j = Math.floor(newY + Player.HEIGHT);
+            for(let i = Math.floor(newX - Player.WIDTH / 2); i < Math.ceil(newX + Player.WIDTH / 2); i++) {
+                if(gameArea.hasCollision(i, j, GameArea.MAIN_LAYOUT)){
+                    return true;
+                }
+            }
+            return false;
+        }
+
+        // Задевает ли нижняя грань игрока блоки с коллизией
+        this.isCollisionDown = (newX, newY) => {
+            let j = Math.floor(newY);
+            for(let i = Math.floor(newX - Player.WIDTH / 2); i < Math.ceil(newX + Player.WIDTH / 2); i++) {
+                if(gameArea.hasCollision(i, j, GameArea.MAIN_LAYOUT)){
+                    return true;
+                }
+            }
+            return false;
+        }
+
+        // Стоит ли на поверхности
         this.onGround = () => {
-            return !this.checkDownCol(x, y - 0.0001);
+            if(this.y - 0.0001 < 0) return true;
+            return this.isCollisionDown(this.x, this.y - 0.0001);
         }
     }
 }
 
 
-Player.ACTION_RADIUS = 12; // Радиус действия игрока
-Player.HEIGHT = 2.8; // "Рост" игрока в блоках
-Player.HALF_WIDTH = 0.75; // Половина ширины игрока в блоках
-Player.SPEED = 15; // Модификатор скорости игрока
-Player.JUMP_SPEED = 31; // Модификатор скорости игрока
+Player.ACTION_RADIUS = 12;      // Радиус действия игрока
+Player.HEIGHT = 2.8;            // "Рост" игрока в блоках
+Player.WIDTH = 1.5;             // Половина ширины игрока в блоках
+Player.SPEED = 15;              // Модификатор скорости игрока
+Player.JUMP_SPEED = 30;         // Модификатор максимальной скорости прыжка игрока

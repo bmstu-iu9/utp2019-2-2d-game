868e3759b33d5d8b7572d9319ee2bd2c20bdd175
Ilya Karkin <mitbus@mail.ru>
2019-07-12 16:29:36 +0300

    back

--- Scripts/controller.js
+++ Scripts/controller.js
@@ -32,7 +32,7 @@ class Controller {
         this.mouse.x = event.pageX;
         this.mouse.y = event.pageY;
         this.mouse.direction.x = this.mouse.x - document.getElementById('canvas').width / 2 - playerPlixelLocateX;
-        this.mouse.direction.y = document.getElementById('canvas').height / 2 + playerPlixelLocateY - this.mouse.y -
+        this.mouse.direction.y = document.getElementById('canvas').height / 2 - playerPlixelLocateY - this.mouse.y -
             Player.HEIGHT * scale * cameraScale / 2;
     }
 

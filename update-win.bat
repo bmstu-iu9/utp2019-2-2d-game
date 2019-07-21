e09f4975dde099996b9ffb525a516a7f0fb33bf0
Маздайщик <a.v.konovalov87@mail.ru>
2019-07-21 17:06:46 +0300

    Скрипты знают ветку Graphical-Engine

--- update-win.bat
+++ update-win.bat
@@ -3,5 +3,6 @@ git checkout gh-pages
 git pull
 git read-tree --prefix=master/ -u master
 git read-tree --prefix=develop/ -u develop
+git read-tree --prefix=Graphical-Engine/ -u Graphical-Engine
 git commit -m "%date% %time%" --allow-empty
 git push

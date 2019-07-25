b040872b0fda4562f04f203ecc2e9f457f718baf
whitequartz <nadim.abdulgani@mail.ru>
2019-07-25 14:08:42 +0300

    Оптимизация шейдеров

--- Scripts/shaders.js
+++ Scripts/shaders.js
@@ -44,17 +44,11 @@ _vertexShader[1] = `
 	uniform vec3 u_translate;
 	uniform mat4 u_projectionMatrix;
 	uniform float u_resolution;
-	uniform float u_light;
-	uniform vec2 u_center;
 
 	varying vec2 v_texCoord;
-	varying float v_light;
-	varying vec2 v_center;
 
 	void main() {
 		v_texCoord = a_texCoord;
-		v_light = u_light;
-		v_center = u_center;
 		vec4 pos = vec4(u_translate + vec3(a_position / u_resolution, 0.0), 1.0);
 		gl_Position = u_projectionMatrix * pos;
 	}`;
@@ -64,20 +58,20 @@ _fragmentShader[1] = `
 
 	uniform sampler2D u_texture0;
 	uniform sampler2D u_texture1;
+	uniform float u_light;
+	uniform vec2 u_center;
 
 	varying vec2 v_texCoord;
-	varying float v_light;
-	varying vec2 v_center;
 
 	void main() {
 		float radius = 250.0;
 		float minAlpha = 0.2;
 		vec4 tex = texture2D(u_texture0, v_texCoord);
 		vec4 lightTex = texture2D(u_texture1, vec2(v_texCoord.x / 2.0 + 1.0 / 36.0, v_texCoord.y / 2.0 + 1.0 / 36.0));
-		vec2 delta = v_center - gl_FragCoord.xy;
+		vec2 delta = u_center - gl_FragCoord.xy;
 		float alpha = clamp(sqrt(delta.x * delta.x + delta.y * delta.y) * (1.0 - minAlpha) / radius + minAlpha,
 			minAlpha, 1.0);
-		gl_FragColor = vec4(tex.rgb * lightTex.x * v_light, tex.a * alpha);
+		gl_FragColor = vec4(tex.rgb * lightTex.x * u_light, tex.a * alpha);
 	}`;
 
 _vertexShader[2] = `
@@ -87,17 +81,11 @@ _vertexShader[2] = `
 	uniform vec3 u_translate;
 	uniform mat4 u_projectionMatrix;
 	uniform float u_resolution;
-	uniform float u_light;
-	uniform float u_sizeBlock;
 
 	varying vec2 v_texCoord;
-	varying float v_light;
-	varying float v_sizeBlock;
 
 	void main() {
 		v_texCoord = a_texCoord;
-		v_light = u_light;
-		v_sizeBlock = u_sizeBlock;
 		vec4 pos = vec4(u_translate + vec3(a_position / u_resolution, 0.0), 1.0);
 		gl_Position = u_projectionMatrix * pos;
 	}`;
@@ -107,19 +95,18 @@ _fragmentShader[2] = `
 
 	uniform sampler2D u_texture0;
 	uniform sampler2D u_texture1;
+	uniform float u_light;
+	uniform float u_sizeBlock;
 	uniform vec4 u_dynamicLight;
 
 	varying vec2 v_texCoord;
-	varying float v_light;
-	varying float v_sizeBlock;
 
 	void main() {
 		vec4 tex = texture2D(u_texture0, v_texCoord);
-		float offset = 1.0 / (v_sizeBlock + 2.0);
-		vec4 lightTex = texture2D(u_texture1, (v_texCoord + offset) / 2.0);
+		vec4 lightTex = texture2D(u_texture1, (v_texCoord + 1.0 / (u_sizeBlock + 2.0)) / 2.0);
 		float radius = u_dynamicLight.p;
 		float maxLight = u_dynamicLight.a;
 		vec2 delta = u_dynamicLight.xy - gl_FragCoord.xy;
 		float light = clamp(maxLight - sqrt(delta.x * delta.x + delta.y * delta.y) * maxLight / radius, 0.0, maxLight);
-		gl_FragColor = vec4(tex.rgb * max(lightTex.x, light) * v_light, tex.a);
+		gl_FragColor = vec4(tex.rgb * max(lightTex.x, light) * u_light, tex.a);
 	}`;

6ddc807b7d5acd81cee85a96981912f2d1118c98
Shaniser <mr.shaniser@gmail.com>
2019-07-25 23:35:53 +0300

    Лишняя ветка удалена

--- update-unix.sh
+++ update-unix.sh
@@ -5,5 +5,6 @@ git pull
 git read-tree --prefix=master/ -u master
 git read-tree --prefix=develop/ -u develop
 git read-tree --prefix=Graphical-Engine/ -u Graphical-Engine
+git read-tree --prefix=Player/ -u Player
 git commit -m "$(date)" --allow-empty
 git push

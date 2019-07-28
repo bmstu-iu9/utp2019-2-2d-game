8db5ef1771a742a0035c8fcbbb19f5948f72fb20
Ilya Karkin <mitbus@mail.ru>
2019-07-29 00:07:56 +0300

    draw

--- Scripts/UI.js
+++ Scripts/UI.js
@@ -1,3 +1,39 @@
+const holeUI = [[0, 0], [_UI.width, _UI.height]]  // Якорь в % + размер в пикселях
+const drawUIElement = (image, part, rect, )
+
+class Sprite {
+    constructor(image, rect, indent, pixelScale) {
+        this.children = [];
+        this.draw = (parent) => {
+            if (parent === undefined) {
+                
+            }
+            const pa = [
+                parent.pa[0] + (parent.pb[0] - parent.pa[0]) * rect.pa.x + ident.pa.x * pixelScale,
+                parent.pa[1] + (parent.pb[1] - parent.pa[1]) * rect.pa.y + ident.pa.y * pixelScale
+            ], pb = [
+                parent.pb[0] + (parent.pb[0] - parent.pa[0]) * rect.pb.x + ident.pb.x * pixelScale,
+                parent.pb[1] + (parent.pb[1] - parent.pa[1]) * rect.pb.y + ident.pb.y * pixelScale
+            ];
+            let ans = [
+                {
+                    'pa': pa,
+                    'pb': pb,
+                    'ta': image[0],
+                    'tb': image[1]
+                }
+            ];
+            for (let i = 0; i < this.children.length; i++) {
+                ans.concat(children[i].draw({
+                    'pa': pa,
+                    'pb': pb
+                }));
+            }
+            return ans;
+        }
+    }
+}
+
 // Возмращает _array по-умолчанию
 const defaultUI = () => {
     const _size = render.getCanvasSize(); // получаем размер экрана

f0a20722df53a11db29f8238a55e5b02af0a5255
Ilya Karkin <mitbus@mail.ru>
2019-08-18 14:16:51 +0300

    prompts1

--- Scripts/prompt.js
+++ Scripts/prompt.js
@@ -2,12 +2,25 @@ const prompts = [
     {
         condition: () => gameArea.get(Math.floor(player.x) - 1, Math.floor(player.y), player.layout) === 17
             || gameArea.get(Math.floor(player.x) + 1, Math.floor(player.y), player.layout) === 17,
-        message: "press s to switch to another layer"
+        message: "press s to switch to another layer."
     },
 
     {
         condition: () => player.bp !== player.maxBP,
-        message: "you are choking underwater. pop up to stop it."
+        message: "you are choking. free yourself from a block to stop it.",
+        time: 3
+    },
+
+    {
+        condition: () => player.sp / player.maxSP < 0.1,
+        message: "you are tired. stop jumping and mining to relax.",
+        time: 5
+    },
+
+    {
+        condition: () => player.hp !== player.maxHP && player.sp !== player.maxSP,
+        message: "you got a damage. keep full stamina to heal.",
+        time: 3
     }
 ];
 
@@ -27,7 +40,6 @@ class PromptSet {
                 time: time
             });
         }
-
         this.addAll = (array) => {
             for (let i = 0; i < array.length; i++) {
                 this.add(array[i].condition, array[i].message, array[i].time);
